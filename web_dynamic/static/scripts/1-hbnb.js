026e93efad1607466bf670638407e516551fff8b
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-11-04 21:47:51 +0100

    completed tasks 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    let selectedAmenities = {};
+
+    $(document).on('change', "input[type='checkbox']", function () {
+        var amenityId = $(this).data('id');
+        var amenityName = $(this).data('name');
+
+        if ($(this).is(":checked")) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        var amenitiesText = Object.values(selectedAmenities).join(', ');
+        $("div.amenities h4").text("Amenities: " + amenitiesText);
+    });
+});
